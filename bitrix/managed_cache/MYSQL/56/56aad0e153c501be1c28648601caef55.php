efe3edf4a4ade6e31bfb1fcb4aca2b8765717573
ИМЯ <smileasya@mail.ru>
2019-07-04 09:20:55 +0300

    Удаление файлов кеша из мастера

--- bitrix/managed_cache/MYSQL/56/56aad0e153c501be1c28648601caef55.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001560190476';
-$dateexpire = '001562782476';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:13:"ADMIN_SECTION";b:1;s:14:"PUBLIC_SECTION";b:1;}}';
-return true;
-?>
--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001560865363';
-$dateexpire = '001560869023';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1560865394;}';
-return true;
-?>
